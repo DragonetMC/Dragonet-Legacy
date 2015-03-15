881c260956749f38d7952094707bb4d15c74e556
QuarkTheAwesome <connorisawesome1123@gmail.com>
2015-03-15 17:11:06 +1100

    Fix NPE in certain circumstances (useItem)
    Issue: NPE when right-clicking air with item or when an attempt to place
    a block fails (for instance, when you're standing in where it needs to
    go)
    Fix: Surround with try/catch.

--- src/main/java/net/glowstone/EventFactory.java
+++ src/main/java/net/glowstone/EventFactory.java
@@ -170,7 +170,10 @@ public final class EventFactory {
 
     public static PlayerInteractEvent onPlayerInteract(Player player, Action action, Block clicked, BlockFace face) {
         //Dragonet-Add
-        org.dragonet.DragonetServer.instance().getRhino().useItem(clicked.getX(), clicked.getY(), clicked.getZ(), face.name(), clicked.getType().name(), player.getName());
+    	try {
+    		org.dragonet.DragonetServer.instance().getRhino().useItem(clicked.getX(), clicked.getY(), clicked.getZ(), face.name(), clicked.getType().name(), player.getName());
+    	} catch (NullPointerException e) {
+    	}
         //Dragonet-End
         return callEvent(new PlayerInteractEvent(player, action, player.getItemInHand(), clicked, face));
     }
