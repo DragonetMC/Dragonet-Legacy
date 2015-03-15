84b03c01322e66b2e8c61312ba548fb7b98b3460
QuarkTheAwesome <connorisawesome1123@gmail.com>
2015-03-15 16:27:54 +1100

    Fix exception when DAPIS doesn't use all hooks
    Error: scope.get on a missing hook returns UniqueTag, which cannot be
    cast to Function.
    Fix: Only attempt to cast to Function if object is NOT UniqueTag. See
    line 95.

--- src/main/java/org/dragonet/rhino/Script.java
+++ src/main/java/org/dragonet/rhino/Script.java
@@ -15,105 +15,91 @@ import org.dragonet.rhino.api.functions.*;
  *
  * @author TheMCPEGamer__
  */
-public class Script
-{
-    public String name = "";
-    
-    public String fullFilePath = "";
-    
-    public String fileContents = "";
-    
-    public File file;
-    
-    public Script(File scriptFile)
-    {       
-        this.name = scriptFile.getName();
-        
-        this.fullFilePath = scriptFile.getAbsolutePath();
-        
-        this.fileContents = getScriptContents(scriptFile);
-        
-        this.file = scriptFile;
-    }
-    
-    public File getFile()
-    {
-        return this.file;
-    }
-    
-    public String getName()
-    {
-        return this.name;
-    }
-    
-    public String getPath()
-    {
-        return this.fullFilePath;
-    }
-    
-    private String getScriptContents(File f)
-    {
-        List<String> scriptContentList = new ArrayList<>();
-        
-        String scriptContents = new String();
-        
-        try 
-        {
-            scriptContentList = Files.readLines(f, Charset.defaultCharset());          
-        }
-        
-        catch(IOException IOe)
-        {
-            System.out.println(IOe.getMessage());
-        }
-            
-        for(String str : scriptContentList)
-        {
-            scriptContents += " " + str;
-        }
-        
-            return scriptContents;
-    }
-    
-    public void runFunction(String func, Object[] params)
-    {  
-        BufferedReader script = null;
-        
-        try
-        {
-            script = new BufferedReader(new FileReader(this.getFile()));
-        }
-        
-        catch(IOException IOe)
-        {
-            System.out.println(Arrays.toString(IOe.getStackTrace()));
-        }
-        
-        Context context = Context.enter();
-        
-	try
-        {
-            ScriptableObject scope = context.initStandardObjects();
-            
-            try
-            {
-                Functions.defineFunctions(scope);
-                context.evaluateReader(scope, script, "script", 1, null);
-            }
-            
-            catch(IOException IOe)
-            {
-                System.out.println(Arrays.toString(IOe.getStackTrace()));
-            }
-            
-            
-            Function fct = (Function)scope.get(func, scope);
-            Object result = fct.call(context, scope, scope, params);
-	} 
-	
-        finally
-        {
-            Context.exit();
+public class Script {
+	public String name = "";
+
+	public String fullFilePath = "";
+
+	public String fileContents = "";
+
+	public File file;
+
+	public Script(File scriptFile) {
+		this.name = scriptFile.getName();
+
+		this.fullFilePath = scriptFile.getAbsolutePath();
+
+		this.fileContents = getScriptContents(scriptFile);
+
+		this.file = scriptFile;
 	}
-    }
+
+	public File getFile() {
+		return this.file;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getPath() {
+		return this.fullFilePath;
+	}
+
+	private String getScriptContents(File f) {
+		List<String> scriptContentList = new ArrayList<>();
+
+		String scriptContents = new String();
+
+		try {
+			scriptContentList = Files.readLines(f, Charset.defaultCharset());
+		}
+
+		catch (IOException IOe) {
+			System.out.println(IOe.getMessage());
+		}
+
+		for (String str : scriptContentList) {
+			scriptContents += " " + str;
+		}
+
+		return scriptContents;
+	}
+
+	public void runFunction(String func, Object[] params) {
+		BufferedReader script = null;
+
+		try {
+			script = new BufferedReader(new FileReader(this.getFile()));
+		}
+
+		catch (IOException IOe) {
+			System.out.println(Arrays.toString(IOe.getStackTrace()));
+		}
+
+		Context context = Context.enter();
+
+		try {
+			ScriptableObject scope = context.initStandardObjects();
+
+			try {
+				Functions.defineFunctions(scope);
+				context.evaluateReader(scope, script, "script", 1, null);
+			}
+
+			catch (IOException IOe) {
+				System.out.println(Arrays.toString(IOe.getStackTrace()));
+			}
+
+			Object function = scope.get(func, scope);
+			if (!(function instanceof UniqueTag)) {
+				Function fct = (Function) function;
+				Object result = fct.call(context, scope, scope, params);
+			}
+		}
+
+		finally {
+			Context.exit();
+		}
+	}
 }
