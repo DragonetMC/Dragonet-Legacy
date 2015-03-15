a5ab3090f173bfd84a04807ed8f88fbc06ec1814
QuarkTheAwesome <connorisawesome1123@gmail.com>
2015-03-15 18:28:12 +1100

    Added function: runConsoleCommand(cmd)
    Will run a command as the console. DO NOT INCLUDE "/" !

--- src/main/java/org/dragonet/rhino/api/functions/Functions.java
+++ src/main/java/org/dragonet/rhino/api/functions/Functions.java
@@ -12,9 +12,9 @@ import org.mozilla.javascript.*;
  */
 public class Functions
 {
-    public static String[] Functions = new String[] {"clientMessage", "addItemInventory", "getCurrentWorld", "setBlock"};
+    public static String[] Functions = new String[] {"clientMessage", "addItemInventory", "getCurrentWorld", "setBlock", "runConsoleCommand"};
     
-    public static Class[] Classes = new Class[] {clientMessage.class, addItemInventory.class, getCurrentWorld.class, setBlock.class};
+    public static Class[] Classes = new Class[] {clientMessage.class, addItemInventory.class, getCurrentWorld.class, setBlock.class, runConsoleCommand.class};
     
     public static void defineFunctions(Scriptable scope)
     {
