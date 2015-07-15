261fbdc443e5070732f4838a7752802c0c9534f8
TheEvilDude <theevildays@gmail.com>
2015-07-15 09:36:49 +0800

    fix upgrade caused code errors

--- Dragonet-core/src/main/java/org/dragonet/net/translator/topc/WindowItemsPacketTranslator.java
+++ Dragonet-core/src/main/java/org/dragonet/net/translator/topc/WindowItemsPacketTranslator.java
@@ -8,6 +8,8 @@ package org.dragonet.net.translator.topc;
 import com.flowpowered.networking.Message;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+import org.dragonet.inventory.ItemList;
 import org.dragonet.inventory.PEInventorySlot;
 import org.dragonet.inventory.PEWindowConstantID;
 import org.dragonet.net.DragonetSession;
@@ -43,9 +45,33 @@ public class WindowItemsPacketTranslator extends PEPacketTranslatorToPC<Translat
             return null;
         }
         this.getSession().getPlayer().getInventory().addItem(recipe.getResult());
-        this.getSession().getServer().getCraftingManager().removeItems(this.getSession().getPlayer().getInventory().getContents(), recipe);
+        this.removeItems(this.getSession().getPlayer().getInventory().getContents(), recipe);
         emptyCrafting();
         return null;
+    }
+    
+        /**
+     * Remove enough items from the given item list to form the given recipe.
+     * @param items The items to remove the ingredients from.
+     * @param recipe A recipe known to match the items.
+     */
+    public void removeItems(ItemStack[] items, Recipe recipe) {
+        //DRAGONET - Implemented removeItems() method for Glowstone. 
+        ItemList lst = new ItemList(items);
+        if (recipe instanceof ShapedRecipe) {
+            ShapedRecipe shaped = (ShapedRecipe) recipe;
+            for (String itemChar : shaped.getShape()) {
+                ItemStack ingredient = shaped.getIngredientMap().get(new Character(itemChar.charAt(0)));
+                if (ingredient == null) {
+                    continue;
+                }
+                lst.tryToRemove(ingredient);
+            }
+        }
+        for(int i = 0; i < items.length; i++){
+            items[i] = lst.getItems().get(i);
+        }
+        //DRAGONET - END
     }
 
     public void emptyCrafting() {
