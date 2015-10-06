c434c0e42a2f2365423fcea3512c21ff60935341
root <root@euve78127.serverprofi24.net>
2015-10-06 09:45:45 +0000

    (WARNING) (EXPERIMENTAL) Removed GloeStone source code from this repo

--- Dragonet-core/src/main/java/net/glowstone/net/message/play/player/PlayerSwingArmMessage.java
+++
@@ -1,9 +0,0 @@
-package net.glowstone.net.message.play.player;
-
-import com.flowpowered.networking.Message;
-import lombok.Data;
-
-@Data
-public final class PlayerSwingArmMessage implements Message {
-
-}
