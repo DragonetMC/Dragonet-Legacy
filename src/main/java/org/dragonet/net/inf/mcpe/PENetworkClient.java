f2e3ee5ab6436d24f70fc9606a1669e71cfa5c04
SenanYT <senandinko@gmail.com>
2015-12-04 15:59:22 +0200

    Really hacky fix for the disconnect bug!

--- src/main/java/org/dragonet/net/inf/mcpe/PENetworkClient.java
+++ src/main/java/org/dragonet/net/inf/mcpe/PENetworkClient.java
@@ -17,7 +17,12 @@ import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import lombok.Getter;
+import net.glowstone.GlowServer;
+import net.glowstone.entity.GlowPlayer;
 import net.glowstone.entity.meta.profile.PlayerProfile;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.dragonet.entity.DragonetPlayer;
 import org.dragonet.net.DragonetSession;
 import org.dragonet.net.packet.Protocol;
 import org.dragonet.net.packet.minecraft.BatchPacket;
@@ -31,98 +36,112 @@ import org.dragonet.net.translator.TranslatorProvider;
 
 public final class PENetworkClient {
 
-    /**
-     * The session which this client binds to.
-     */
-    @Getter
-    private MCPESession session;
-    
-    @Getter
-    private final String raknetIdentifier;
-    
-    @Getter
-    private final InetSocketAddress remoteAddress;
-    
-    @Getter
-    private final long clientID;
-    
-    @Getter
-    private final RakNetInterface inf;
-    
-    private String username;
+	/**
+	 * The session which this client binds to.
+	 */
+	@Getter
+	private MCPESession session;
 
-    public PENetworkClient(RakNetInterface inf, String raknetIdentifier, InetSocketAddress remoteAddress, long clientID) {
-        this.raknetIdentifier = raknetIdentifier;
-        this.inf = inf;
-        this.remoteAddress = remoteAddress;
-        this.clientID = clientID;
-    }
+	@Getter
+	private final String raknetIdentifier;
 
-    public void processPacketBuffer(byte[] buffer) {
-        PEPacket packet = Protocol.decode(buffer);
-        if (packet == null) {
-            return;
-        }
-        handlePacket(packet);
-    }
-    
-    public void handlePacket(PEPacket packet){
-        System.out.println("Received Packet: " + packet.getClass().getSimpleName());
-        switch (packet.pid()) {
-            case PEPacketIDs.LOGIN_PACKET:
-                LoginPacket packetLogin = (LoginPacket) packet;
-                this.username = packetLogin.username;
-                
-                BaseTranslator translator = TranslatorProvider.getByPEProtocolID(packetLogin.protocol1, packetLogin.protocol2);
-                if (!(translator instanceof BaseTranslator)) {
-                    LoginStatusPacket pkLoginStatus = new LoginStatusPacket();
-                    pkLoginStatus.status = LoginStatusPacket.LOGIN_FAILED_CLIENT;
-                    this.sendPacket(pkLoginStatus);
-                    this.disconnect("Unsupported game version! ");
-                    break;
-                }
-                session = new MCPESession(inf.getSesMan().getServer(), this, translator);
+	@Getter
+	private final InetSocketAddress remoteAddress;
 
-                LoginStatusPacket pkLoginStatus = new LoginStatusPacket();
-                pkLoginStatus.status = LoginStatusPacket.LOGIN_SUCCESS;
-                this.sendPacket(pkLoginStatus);
+	@Getter
+	private final long clientID;
 
-                inf.getSesMan().getServer().getLogger().info("Accepted connection by [" + this.username + "]. ");
+	@Getter
+	private final RakNetInterface inf;
 
-                Matcher matcher = DragonetSession.PATTERN_USERNAME.matcher(this.username);
-                if (!matcher.matches()) {
-                    this.disconnect("Bad username! ");
-                    break;
-                }
+	private String username;
 
-                session.setPlayer(new PlayerProfile(this.username, UUID.nameUUIDFromBytes(("OfflinePlayer:" + this.username).getBytes(StandardCharsets.UTF_8))));
-                break;
-            case PEPacketIDs.BATCH_PACKET:
-                BatchPacket packetBatch = (BatchPacket) packet;
-                if(packetBatch.packets == null || packetBatch.packets.isEmpty()) break;
-                for(PEPacket pk : packetBatch.packets){
-                    this.handlePacket(pk);
-                }
-                break;
-            default:
-                if (session == null) {
-                    disconnect("Network error! ");
-                    return;
-                }
-                session.onPacketReceived(packet);
-        }
-    }
-    
-    public void disconnect(String reason){
-        this.sendPacket(new DisconnectPacket(reason));
-        if(session != null) session.onDisconnect();
-    }
-    
-    public void sendPacket(PEPacket packet){
-        sendPacket(packet, 2);
-    }
-    
-    public void sendPacket(PEPacket packet, int reliability){
-        inf.sendPacket(raknetIdentifier, packet, true, packet.isShouldSendImmidate());
-    }
+	public PENetworkClient(RakNetInterface inf, String raknetIdentifier, InetSocketAddress remoteAddress, long clientID) {
+		this.raknetIdentifier = raknetIdentifier;
+		this.inf = inf;
+		this.remoteAddress = remoteAddress;
+		this.clientID = clientID;
+	}
+
+	public void processPacketBuffer(byte[] buffer) {
+		PEPacket packet = Protocol.decode(buffer);
+		if (packet == null) {
+			return;
+		}
+		handlePacket(packet);
+	}
+
+	public void handlePacket(PEPacket packet){
+		System.out.println("Received Packet: " + packet.getClass().getSimpleName());
+		switch (packet.pid()) {
+			case PEPacketIDs.LOGIN_PACKET:
+				LoginPacket packetLogin = (LoginPacket) packet;
+				this.username = packetLogin.username;
+
+				BaseTranslator translator = TranslatorProvider.getByPEProtocolID(packetLogin.protocol1, packetLogin.protocol2);
+				if (!(translator instanceof BaseTranslator)) {
+					LoginStatusPacket pkLoginStatus = new LoginStatusPacket();
+					pkLoginStatus.status = LoginStatusPacket.LOGIN_FAILED_CLIENT;
+					this.sendPacket(pkLoginStatus);
+					this.disconnect("Unsupported game version! ");
+					break;
+				}
+				session = new MCPESession(inf.getSesMan().getServer(), this, translator);
+
+				LoginStatusPacket pkLoginStatus = new LoginStatusPacket();
+				pkLoginStatus.status = LoginStatusPacket.LOGIN_SUCCESS;
+				this.sendPacket(pkLoginStatus);
+
+				inf.getSesMan().getServer().getLogger().info("Accepted connection by [" + this.username + "]. ");
+
+				Matcher matcher = DragonetSession.PATTERN_USERNAME.matcher(this.username);
+				if (!matcher.matches()) {
+					this.disconnect("Bad username! ");
+					break;
+				}
+
+				session.setPlayer(new PlayerProfile(this.username, UUID.nameUUIDFromBytes(("OfflinePlayer:" + this.username).getBytes(StandardCharsets.UTF_8))));
+				break;
+			case PEPacketIDs.BATCH_PACKET:
+				BatchPacket packetBatch = (BatchPacket) packet;
+				if(packetBatch.packets == null || packetBatch.packets.isEmpty()) break;
+				for(PEPacket pk : packetBatch.packets){
+					this.handlePacket(pk);
+				}
+				break;
+			default:
+				if (session == null) {
+					disconnect("Network error! ");
+					return;
+				}
+				session.onPacketReceived(packet);
+		}
+	}
+
+	public void disconnect(String reason){
+		kick(reason);
+		this.sendPacket(new DisconnectPacket(reason));
+		if(session != null) session.onDisconnect();
+	}
+
+	public void kick(String reason) {
+
+		GlowPlayer glowPlayer = session.getPlayer();
+		if(glowPlayer instanceof DragonetPlayer) {
+			Player player = Bukkit.getPlayer(glowPlayer.getUniqueId());
+			try {
+				player.kickPlayer(reason);
+			} catch(Exception e) {
+			}
+		}
+
+	}
+
+	public void sendPacket(PEPacket packet){
+		sendPacket(packet, 2);
+	}
+
+	public void sendPacket(PEPacket packet, int reliability){
+		inf.sendPacket(raknetIdentifier, packet, true, packet.isShouldSendImmidate());
+	}
 }
