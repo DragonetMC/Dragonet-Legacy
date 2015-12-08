49827d909d6ea12eb021703c91c358d909130fea
DefinitlyEvil <theevildays@gmail.com>
2015-12-08 12:50:49 +0800

    correct PositionRotationMessageTranslator to support relative updates

--- src/main/java/org/dragonet/net/translator/tope/PositionRotationMessageTranslator.java
+++ src/main/java/org/dragonet/net/translator/tope/PositionRotationMessageTranslator.java
@@ -7,7 +7,7 @@ package org.dragonet.net.translator.tope;
 
 import net.glowstone.net.message.play.game.PositionRotationMessage;
 import org.dragonet.net.DragonetSession;
-import org.dragonet.net.packet.minecraft.MoveEntitiesPacket;
+import org.dragonet.net.packet.minecraft.MovePlayerPacket;
 import org.dragonet.net.packet.minecraft.PEPacket;
 import org.dragonet.net.translator.MessageTranslatorToPE;
 import org.dragonet.net.translator.Translator_v0_11;
@@ -18,23 +18,21 @@ public class PositionRotationMessageTranslator extends MessageTranslatorToPE<Tra
         super(translator, session);
     }
 
-    //Memo: Not working yet, idk why. :( 
     @Override
     public PEPacket[] handleSpecific(PositionRotationMessage packet) {
         if (this.getSession().getPlayer() == null) {
             return null;
         }
-        //Hack: Yaw and pitch set to 0.0f
-        //MovePlayerPacket pkMovePlayer = new MovePlayerPacket(this.getSession().getPlayer().getEntityId(), (float) msgPosRot.x, (float) msgPosRot.y, (float) msgPosRot.z, 0.0f, 0.0f, 0.0f, true);
-        MoveEntitiesPacket.MoveEntityData d = new MoveEntitiesPacket.MoveEntityData();
-        d.eid = this.getSession().getPlayer().getEntityId();
-        d.x = (float) packet.x;
-        d.y = (float) packet.y;
-        d.z = (float) packet.z;
-        d.yaw = 0.0f;
-        d.pitch = 0.0f;
-        MoveEntitiesPacket pkMoveEntity = new MoveEntitiesPacket(new MoveEntitiesPacket.MoveEntityData[]{d});
-        return new PEPacket[]{pkMoveEntity};
+        MovePlayerPacket pkMovePlayer = new MovePlayerPacket(
+                this.getSession().getPlayer().getEntityId(), 
+                (packet.flags & 0x01) > 0 ? (float)(this.getSession().getPlayer().getLocation().getX() + packet.x) : (float) packet.x, 
+                (packet.flags & 0x02) > 0 ? (float)(this.getSession().getPlayer().getLocation().getY() + packet.y) : (float) packet.y, 
+                (packet.flags & 0x04) > 0 ? (float)(this.getSession().getPlayer().getLocation().getX() + packet.z) : (float) packet.z, 
+                (packet.flags & 0x08) > 0 ? this.getSession().getPlayer().getLocation().getYaw() + packet.rotation : packet.rotation, 
+                (packet.flags & 0x10) > 0 ? this.getSession().getPlayer().getLocation().getPitch() + packet.pitch : packet.pitch,  
+                (packet.flags & 0x08) > 0 ? this.getSession().getPlayer().getLocation().getYaw() + packet.rotation : packet.rotation, 
+                true);
+        return new PEPacket[]{pkMovePlayer};
     }
 
 }
